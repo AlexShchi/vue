a5d7cc5484f916ac4f108da5d0bd950598957008
Алексей <shchipilex@mail.ru>
2021-04-18 16:02:50 +0300

    Finished first project, started vue3 project.

--- first-project/src/components/TodoItem.vue
+++ first-project/src/components/TodoItem.vue
@@ -1,51 +1,61 @@
 <template>
-  <li>
+    <li>
     <span v-bind:class="{ done:todo.completed }">
       <input type="checkbox" v-on:change="todo.completed = !todo.completed">
-      <strong>{{index + 1}}</strong>
-      {{todo.title}}
+      <strong>{{ index + 1 }}</strong>
+      {{ todo.title | uppercase }}
     </span>
-    <button class="rm"
-            v-on:click="$emit('remove-todo', todo.id)"
-     >&times;</button>
-  </li>
+        <button class="rm"
+                v-on:click="$emit('remove-todo', todo.id)"
+        >&times;
+        </button>
+    </li>
 </template>
 
 
 <script>
 export default {
-  props: {
-    todo: {
-      type: Object,
-      required: true
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        },
+        index: Number
     },
-    index: Number
-  }
+    filters: {
+        uppercase(value){
+            return value.toUpperCase();
+        }
+    }
 }
 </script>
 
 
 <style>
-  li{
+li {
     border: 1px solid #ccc;
     display: flex;
     justify-content: space-between;
     padding: 0.5rem 2rem;
     margin-bottom: 1rem;
-  }
-  .rm{
+}
+
+.rm {
     background: red;
     color: #fff;
     border-radius: 50%;
     font-weight: bold;
-  }
-  input{
+}
+
+input {
     margin-right: 1rem;
-  }
-  .done{
+}
+
+.done {
     text-decoration: line-through;
-  }
-  button{
-    border:none;
-  }
+}
+
+button {
+    border: none;
+}
 </style>
--- first-project/src/router.js
+++ first-project/src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 
 export default new Router({
     mode: 'history',
-    rotes: [
+    routes: [
         {
             path: '/',
             component: Home
